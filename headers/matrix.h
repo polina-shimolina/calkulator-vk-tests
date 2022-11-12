922a119476052c3b75c05572a33e7a4c49a1c7e7
polina-shimolina <64307985+polina-shimolina@users.noreply.github.com>
2022-11-13 00:37:49 +0300

    Update matrix.h

--- headers/matrix.h
+++ headers/matrix.h
@@ -4,57 +4,52 @@
 
 #ifndef MATRIX_CALCULATOR_MATRIX_H
 #define MATRIX_CALCULATOR_MATRIX_H
-#include <ostream>
-#include "vector.h"
 
-class matrix {
-    friend matrix operator - (const matrix&);
-    friend matrix operator + (const matrix&, const matrix&);
-    friend matrix operator * (const matrix&, const matrix&);
-    friend matrix operator * (const matrix&, const float&);
-    friend matrix operator * (const float&, const matrix&);
-    friend matrix operator - (const matrix&, const matrix&);
-    friend std::ostream& operator << (std::ostream&, const matrix&);
-    friend matrix operator * (const matrix&, const vector&);
-    friend matrix operator * (const vector&, const matrix&);
-    friend matrix operator + (const matrix&, const float&);
-    friend matrix operator + (const float&, const matrix&);
-    friend matrix operator + (const matrix&, const vector&);
-    friend matrix operator + (const vector&, const matrix&);
-    friend matrix operator - (const matrix&, const vector&);
-    friend matrix operator + (const float&, const matrix&);
-    friend matrix operator + (const matrix&, const float&);
-    friend matrix operator - (const matrix&, const float&);
+#include <iostream>
+#include "Vector.h"
 
+class Matrix {
+    friend Matrix operator - (const Matrix& matrix);
+    friend Matrix operator + (const Matrix& matrix1, const Matrix& matrix2);
+    friend Matrix operator * (const Matrix& matrix1, const Matrix& matrix2);
+    friend Matrix operator * (const Matrix& m, const float& value);
+    friend Matrix operator * (const float& value, const Matrix& matrix);
+    friend Matrix operator - (const Matrix& matrix1, const Matrix& matrix2);
+    friend std::ostream& operator << (std::ostream& s, const Matrix& m);
+    friend Matrix operator * (const Matrix& m, const Vector& v);
+    friend Matrix operator * (const Vector& v, const Matrix& m);
+    friend Matrix operator + (const Matrix& m, const float& f);
+    friend Matrix operator + (const float& f, const Matrix& m);
+    friend Matrix operator + (const Matrix& m, const Vector& v);
+    friend Matrix operator + (const Vector& v, const Matrix& m);
+    friend Matrix operator - (const Matrix& m, const Vector& v);
+    friend Matrix operator - (const Matrix& m, const float& f);
 private:
-    int M, N;
-    float** array;
+    size_t lines, columns;
+    float** array{};
 public:
-    matrix();
-    matrix(int, int);
-    matrix(float**, int, int);
-    matrix(vector*, int number);
-    matrix(vector);
-    ~matrix();
-    //matrix(float*, int); //--
-
-    int get_M();
-    int get_N();
-    matrix element_multiply(matrix, matrix);
+    Matrix();
+    Matrix(size_t _m, size_t _n);
+    Matrix(float** values, size_t _m, size_t _n);
+    Matrix(Vector* v, size_t number);
+    Matrix(Vector v);
+    ~Matrix();
+    size_t get_lines() const;
+    size_t get_columns() const;
+    Matrix element_multiply(const Matrix& matrix1, const Matrix& matrix2);
 
     float** get_array();
-    //float get_element(int, int);
-    float* get_diagonal(int);
-    float* get_line(int);
-    float* get_column(int);
-    matrix get_transposed();
+    float* get_diagonal(int type);
+    float* get_line(int num);
+    float* get_column(int num);
+    Matrix get_transposed();
     float get_det();
-    void set_line(float*, int);
-    void set_column(float*, int);
+    void set_line(const float* temp_array, int num);
+    void set_column(const float* temp_array, int num);
 
-    matrix inversion();
+    Matrix inversion();
 
-    double operator()(int index, int index2);
+    float& operator()(int index, int index2);
 };
 
 
